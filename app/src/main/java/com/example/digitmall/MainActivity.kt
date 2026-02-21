6000ff64c558132496a397fcc58b3ee33a3123f2
Sandro Mtchedlidze <s.mchedlidze2004@gmail.com>
2026-02-21 22:08:58 +0400

    navigation added

--- app/src/main/java/com/example/digitmall/MainActivity.kt
+++ app/src/main/java/com/example/digitmall/MainActivity.kt
@@ -11,37 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.digitmall.navigation.AppNavigation
 import com.example.digitmall.ui.theme.DigitMallTheme
+import com.example.ui.theme.MallTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DigitMallTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MallTheme {
+                AppNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DigitMallTheme {
-        Greeting("Android")
     }
 }
